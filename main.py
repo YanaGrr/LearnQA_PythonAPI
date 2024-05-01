2cac678de3f4336783ead1f3281f18a9e950d541
YanaGrr <johannag.nn@gmail.com>
2024-05-01 17:30:39 +0300

    Ex4: GET-запрос

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
+import requests
+
+#Задание 3: Напишите программу, которая выводит: “Hello from <ваше_имя>”.
 print("Hello from Yana")
+
+#Задание 4: Создать скрипт, который отправляет GET-запрос по адресу: https://playground.learnqa.ru/api/get_text
+response = requests.get("https://playground.learnqa.ru/api/get_text")
+print(response.text)
